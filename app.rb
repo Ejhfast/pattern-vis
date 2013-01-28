55f02a21df1efb992cc3dd49a50b1d3c40f012c5
Ethan Fast <ethan@Ethans-MacBook-Air-2.local>
2013-01-28 07:48:21 -0800

    first commit

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'rubygems'
+require 'sinatra'
+require 'haml'
+
+# Helpers
+require './lib/render_partial'
+
+# Set Sinatra variables
+set :app_file, __FILE__
+set :root, File.dirname(__FILE__)
+set :views, 'views'
+set :public_folder, 'public'
+set :haml, {:format => :html5} # default Haml format is :xhtml
+
+def get_data
+  data_file = "../data2"
+  Marshal.load(File.new(data_file).to_a.join)
+end
+
+# Application routes
+get '/' do
+  @data = get_data
+  haml :index, :layout => :'layouts/application'
+end
+
+get '/about' do
+  haml :about, :layout => :'layouts/page'
+end
---
+++ public/javascripts/application.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	// Open external links in a new window
+	hostname = window.location.hostname
+	$("a[href^=http]")
+	  .not("a[href*='" + hostname + "']")
+	  .addClass('link external')
+	  .attr('target', '_blank');
+	
+	$('.showHide').click(function(){
+		$(this).parent().find('.code').toggle()
+	});
+
+});
