ed63e8dde27f4fc9b05e4f736c79e8783b4bbf34
Ethan Fast <ethan@Ethans-Mac-Pro.local>
2013-01-28 09:00:14 -0800

    blank

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ set :public_folder, 'public'
 set :haml, {:format => :html5} # default Haml format is :xhtml
 
 def get_data
-  data_file = "data/data4"
+  data_file = "data/data5"
   Marshal.load(File.new(data_file).to_a.join)
 end
 
